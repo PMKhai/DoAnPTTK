86f170c061cffab8dc6050c37b1ac9fb5d97924a
Khai Pham <minhkhai3012@gmail.com>
2019-06-19 17:37:17 +0700

    check age when add new reader

--- ViewModel/AddingReaderViewModel.cs
+++ ViewModel/AddingReaderViewModel.cs
@@ -21,6 +21,9 @@ namespace QLTV_MVVM.ViewModel
         private string _Address;
         public string Address { get => _Address; set { _Address = value; OnPropertyChanged(); } }
 
+        private string _PhoneNumber;
+        public string PhoneNumber { get => _PhoneNumber; set { _PhoneNumber = value; OnPropertyChanged(); } }
+
         private DateTime _DayOfbirth = System.DateTime.Today;
         public DateTime DayOfbirth { get => _DayOfbirth; set { _DayOfbirth = value; OnPropertyChanged(); } }
 
@@ -36,29 +39,61 @@ namespace QLTV_MVVM.ViewModel
                 var address = Address;
                 var dayOfBirth = DayOfbirth;
                 var redistrationDate = RegistrationDate;
+                var phonenumber = PhoneNumber;
+
+                if (name == null || address == null 
+                    || phonenumber == null)
+                {
+                    MessageBox.Show("Nhập thiếu thông tin", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
 
                 DocGia docGia = new DocGia();
                 docGia.HoTen = name;
                 docGia.DiaChi = address;
                 docGia.NgaySinh = dayOfBirth;
                 docGia.NgayTaoThe = redistrationDate;
-                DataProvider.Ins.DB.DocGias.Add(docGia);
-                if (DataProvider.Ins.DB.SaveChanges() == 1)
+                docGia.SDT = phonenumber;
+                
+                if (KiemTraTuoi(dayOfBirth))
                 {
-                    IsAddingReader = true;
-                    Name = "";
-                    Address = "";
-                    DayOfbirth = System.DateTime.Today;
-                    RegistrationDate = System.DateTime.Today;
+                    DataProvider.Ins.DB.DocGias.Add(docGia);
+                    if (DataProvider.Ins.DB.SaveChanges() == 1)
+                    {
+                        IsAddingReader = true;
+                        Name = "";
+                        Address = "";
+                        DayOfbirth = System.DateTime.Today;
+                        RegistrationDate = System.DateTime.Today;
 
-                    p.Close();    
+                        p.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lỗi không được đọc giả");
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("Lỗi không được đọc giả");
+                    MessageBox.Show("Độ tuổi không hợp lệ!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
                 }
             });
+        }
 
+        Boolean KiemTraTuoi (DateTime dateOfBirth)
+        {
+            var age = Math.Ceiling((DateTime.Today - dateOfBirth).TotalDays / 365.25);
+
+            var thamSos = DataProvider.Ins.DB.ThamSoes.ToArray();
+            var lonhon = Double.Parse(thamSos[0].GiaTri);
+            var nhohon = Double.Parse(thamSos[1].GiaTri);
+
+            if(lonhon <= age && age <= nhohon)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
